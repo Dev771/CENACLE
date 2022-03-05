f0d5e683fcfc0ffb5d89fcc91027cbadbdef34c5
NAMANBHATEJA57 <namanbhateja57@gmail.com>
2022-03-05 21:36:41 +0530

    icon

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -3,6 +3,7 @@ import { useLocation, useNavigate, Link } from 'react-router-dom';
 import { Badge, Avatar, Button } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 import decode from 'jwt-decode';
+import logo from '../../img/logo4.svg'
 
 import './styles.css';
 
@@ -49,6 +50,14 @@ const NavBar = () => {
             <div className="container">
                 <h2 className='headingg'>
                 <span className="logo" onClick={handleWhy}>
+                {/* <img src={logo} className="loggo" alt='' /> */}
+                <svg version="1.1" id="Layer_3" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                    viewBox="0 0 1080 1080" >
+                <polygon points="709.94,834.34 767.67,934.34 312.33,934.34 84.65,540 312.33,145.66 767.67,145.66 709.94,245.66 370.06,245.66 
+                    200.12,540 370.06,834.34 "/>
+                <polygon points="814.55,540 677.27,777.76 402.73,777.76 460.46,677.76 619.54,677.76 699.08,540 619.54,402.23 460.46,402.23 
+                    402.73,302.23 677.27,302.23 "/>
+                </svg>
                     Cenacle
                 </span>
                 </h2>
