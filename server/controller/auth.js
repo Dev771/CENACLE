0d12a218a2ba7ee8c8e75e712ec8613dcd45597e
Dev771 <devgarg771@gmail.com>
2022-10-20 11:55:13 +0530

    bycript

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -1,4 +1,4 @@
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import userSchema from "../model/userSchema.js";
 import {email_Confirmation} from './email.js';
